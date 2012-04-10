2fb09e6a9801512398e6874d73808d73c2029309
Oliver Gierke <info@olivergierke.de>
2012-04-10 17:35:35 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,26 @@
+# Spring Data multi-store tests
+
+Sample project to allow testing that the various Spring Data modules can be used together. It consists of a set of Maven profiles that can be used to run the test case against a variety of dependency setups. The test case consists of a domain class and repository for Spring Data JPA, MongoDB and Neo4J.
+
+## Dependency sets
+
+The profiles can roughly be grouped into some defining the dependency versions for the Spring Data modules as well as ones to define the Spring version to be used. By default the latest snapshot versions of the next Spring Data milestones will be used as well as the snapshot version of the next major Spring version (3.2 as of the time of writing).
+
+## Profiles
+
+- *snapshots* - (default) Includes snapshots of upcoming major versions of Spring Data modules
+- *bugfix-snapshots* - Includes snapshots of the next bugfix releases of Spring Data modules
+- *milestones* - Includes latest milestones of upcoming versions of Spring Data modules
+- *releases* - Includes latest release versions of Spring Data modules
+- *latest-releases* - Includes latest release versions (either GA or milestones) of Spring Data modules
+
+- *fix-spring* - Fixes Spring dependencies to a version (requires one of the following profiles to be activated as well)
+- *3.0* - Includes latest Spring version of the 3.0 branch
+- *3.1* - Includes latest Spring version of the 3.1 branch
+
+## Usage
+The project can be built with profiles activated to produce the wanted set of dependencies. E.g. a Maven run with:
+
+mvn clean install -Pmilestones,fix-spring,3.1
+
+Would run the project against the latest milestones of the Spring Data modules as well as Spring 3.1.1. 
---
+++ src/test/java/org/springframework/data/example/ApplicationIntegrationTest.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright 2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.example;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.example.jpa.User;
+import org.springframework.data.example.jpa.UserRepository;
+import org.springframework.data.example.mongodb.Person;
+import org.springframework.data.example.mongodb.PersonRepository;
+import org.springframework.data.example.neo4j.Movie;
+import org.springframework.data.example.neo4j.MovieRepository;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * 
+ * @author Oliver Gierke
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:META-INF/spring/application-context.xml")
+@TransactionConfiguration(transactionManager = "jpaTransactionManager")
+public class ApplicationIntegrationTest {
+
+	@Autowired
+	UserRepository userRepository;
+
+	@Autowired
+	PersonRepository personRepository;
+
+	@Autowired
+	MovieRepository movieRepository;
+
+	@Test
+	@Transactional
+	public void workWithJpaEntity() {
+
+		User user = new User();
+		user.firstname = "Dave";
+		user.lastname = "Matthews";
+
+		userRepository.save(user);
+
+		List<User> result = userRepository.findByLastname("Matthews");
+		assertThat(result.size(), is(1));
+		assertThat(result.get(0), is(user));
+	}
+
+	@Test
+	public void workWithMongoEntity() {
+
+		personRepository.deleteAll();
+
+		Person person = new Person();
+		person.firstname = "Dave";
+		person.lastname = "Matthews";
+
+		person = personRepository.save(person);
+
+		List<Person> result = personRepository.findByLastname("Matthews");
+		assertThat(result.size(), is(1));
+		assertThat(result.get(0).id, is(person.id));
+	}
+
+	@Test
+	public void wirkWithNeo4JEntity() {
+
+		movieRepository.deleteAll();
+
+		Movie movie = new Movie();
+		movie.title = "The usual suspects";
+
+		movie = movieRepository.save(movie);
+
+		List<Movie> result = movieRepository.findByTitle("The usual suspects");
+		assertThat(result.size(), is(1));
+		assertThat(result.get(0).id, is(movie.id));
+	}
+}
